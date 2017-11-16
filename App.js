5f5adc067a04a0b3311e03bef50aa80f46d41da7
tius1234 <tius1234@nate.com>
2017-11-16 10:03:45 +0900

    아이콘 추가 및 API 연동
    아직 API 키 승인이 나지 않아, 정상 호출은 되지 않음.

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from './Weather';
+
+const API_KEY = "eab8b7299ae911a55857888d58debae9";
 
 export default class App extends Component {
 	state = {
-		isLoaded: true
+		isLoaded: false,
+		error: null,
+		temperature: null,
+		name: null
+	};
+	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				this._getWeather(position.coords.latitude, position.coords.longitude);
+			},
+			error => {
+				this.setState({
+					error: error
+				});
+			}
+		);
 	}
 
+	_getWeather = (lat, lon) => {
+		fetch('http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}')
+			.then(response => response.json())
+			.then(json => {
+				console.log(json);
+				this.setState({
+					temperature: json.main.temp,
+					name: json.weather[0].main,
+					isLoaded: true
+				});
+			});
+	};
+
   render() {
-		const { isLoaded } = this.state;
+		const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
+				<StatusBar hidden={true} />
 				{ isLoaded ? <Weather/> : (
 					<View style={ styles.loading }>
 						<Text style={ styles.loadingText }>Getting the fucking weather</Text>
+						{error ? <Text style={styles.errorText}>{error}</Text> : null}
 					</View>
 				)}
       </View>
@@ -26,6 +58,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+	errorText: {
+  	color: "red",
+		backgroundColor: "transparent",
+		marginBottom: 40
+	},
 	loading: {
   	flex: 1,
   	backgroundColor: '#FDF6AA',
--- Weather.js
+++ Weather.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo';
+import { Ionicons } from "@expo/vector-icons";
 
 export default class Weather extends Component {
 	render() {
@@ -10,7 +11,7 @@ export default class Weather extends Component {
 				style={styles.container}
 			>
 				<View style={styles.upper}>
-					<Text>Icon here!</Text>
+					<Ionicons color="white" size={144} name="ios-rainy" />
 					<Text style={styles.temp}>35</Text>
 				</View>
 				<View style={styles.lower}>
@@ -29,7 +30,8 @@ const styles = StyleSheet.create({
 	upper: {
 		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'center'
+		justifyContent: 'center',
+		backgroundColor: "transparent"
 	},
 	temp: {
 		fontSize: 38,
